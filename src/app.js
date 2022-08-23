cfe4871ad0d94ab8875065d06f33dac3083de203
Miguel Rodriguez <7migro11@gmail.com>
2022-08-22 23:32:09 -0700

    file structure

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <main>
+          <div>Test Of Life</div>
+        </main>
+      </>
+    );
+  }
+}
+
+export default App;
